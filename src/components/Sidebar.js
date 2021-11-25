55297fd0a25d8e4c738eb6936520c43fce7b3956
elsa <elsa.dessarps@gmail.com>
2021-11-25 14:31:39 +0100

    Refactor : change header sidebar style (& add btn to slide in / out sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,11 @@
 import Button from './Button';
+/* import icon */
+import { AiOutlineMenuFold } from "react-icons/ai";
 
 const Sidebar = () => {
     /* variables */
     const name = 'User name';
+    const slideState = 'in';
 
     /* redirections */
     const goToProfile = (event) => {
@@ -14,30 +17,38 @@ const Sidebar = () => {
         console.log('nouvelle recette');
     }
 
+    /* Slide In / Out btn */
+
+
     return (
         <div className="sidebar">
-            <h2>Hello {name} !</h2>
-            <p>Bon Appétit !</p>
-            <nav className="sidebar-nav">
-                <Button 
-                    onClick={ addRecipe }
-                    className="sidebar-nav__btn sidebar-nav__btn--create"
-                    btnText='Créer une Recette'
-                    btnType='addRecipe'
-                />
-                <Button 
-                    onClick={ goToProfile }
-                    className="sidebar-nav__btn sidebar-nav__btn--profil"
-                    btnText='Mon Profil'
-                    btnType='profile'
-                />
-                <Button 
-                    onClick={ goToProfile }
-                    className="sidebar-nav__btn sidebar-nav__btn--search"
-                    btnText='Recherche'
-                    btnType='search'
-                />
-            </nav>
+            <button className="btn-slide">
+                <AiOutlineMenuFold />
+            </button>
+            <div className="sidebar-content">
+                <h2>Hello {name} !</h2>
+                <p>Bon Appétit !</p>
+                <nav className="sidebar-content-nav">
+                    <Button 
+                        onClick={ addRecipe }
+                        className="sidebar-content-nav__btn sidebar-content-nav__btn--create"
+                        btnText='Créer une Recette'
+                        btnType='addRecipe'
+                    />
+                    <Button 
+                        onClick={ goToProfile }
+                        className="sidebar-content-nav__btn sidebar-content-nav__btn--profil"
+                        btnText='Mon Profil'
+                        btnType='profile'
+                    />
+                    <Button 
+                        onClick={ goToProfile }
+                        className="sidebar-content-nav__btn sidebar-content-nav__btn--search"
+                        btnText='Recherche'
+                        btnType='search'
+                    />
+                </nav>
+            </div>
         </div>
     )
 }
