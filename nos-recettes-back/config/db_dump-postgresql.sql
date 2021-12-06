5699b043607fe2f6852bd23e2279966902ff2bd8
elsa <elsa.dessarps@gmail.com>
2021-12-06 12:04:44 +0100

    Feat : start Database dump & connection

---
+++ nos-recettes-back/config/db_dump-postgresql.sql
@@ -0,0 +1,49 @@
+CREATE TABLE users 
+(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE, 
+    pass VARCHAR(255) NOT NULL, 
+    is_admin BOOLEAN DEFAULT false,
+    is_active BOOLEAN DEFAULT true,
+    lastname VARCHAR(100) NOT NULL,
+    firstname VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE ingredients 
+(
+    id SERIAL PRIMARY KEY,
+	name VARCHAR(100)
+);
+
+CREATE TABLE recipes 
+(
+    id SERIAL PRIMARY KEY,
+    id_user INT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+	duration VARCHAR(10),
+    title VARCHAR(100) NOT NULL,
+    preparation TEXT[] NOT NULL,
+    img VARCHAR(250),
+	category VARCHAR(100)[],
+    CONSTRAINT fk_id_user_r
+        FOREIGN KEY (id_user)
+        REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE recipe_ingredients 
+(
+    id SERIAL PRIMARY KEY,
+    id_recipe INT NOT NULL,
+	id_ingredient INT NOT NULL,
+	quantity INT NOT NULL,
+	unit VARCHAR(10),
+    CONSTRAINT fk_id_recipe_ri
+        FOREIGN KEY (id_recipe)
+        REFERENCES recipes(id) ON DELETE CASCADE,
+    CONSTRAINT fk_id_ingredient_ri
+        FOREIGN KEY (id_ingredient)
+        REFERENCES ingredients(id)
+);
+INSERT INTO users (username, email, pass, is_admin, is_active, lastname, firstname) VALUES
+('admin', 'admin@email.com', '1Azer', true, true, 'admin', '1');
