183afdcb556ffcca5b2ba73957c4f0704c01c96d
Sunpadow54 <elsa.dessarps@gmail.com>
2022-01-24 16:46:59 +0100

    Modif (Profile page): modif style of profile page

--- nos-recettes-front/src/pages/Home/Profile/Profile.js
+++ nos-recettes-front/src/pages/Home/Profile/Profile.js
@@ -1,8 +1,13 @@
 import useFetch from "../../../apiFetch/useFetch";
 /* Import Style */
 import "./profile.scss";
+/* Import Icons */
+import { IoRestaurantOutline } from "react-icons/io5";
+
 // Import components
 import ProfileCard from "../../../components/ProfileCard/ProfileCard";
+import HeaderCard from "../../../components/RecipeCards/HeaderCard";
+import IngredientsList from "../../../components/RecipeCards/IngredientsList";
 
 
 function Profile() {
@@ -13,7 +18,13 @@ function Profile() {
 		method: "GET",
 	});
 
+	const getRecipes = useFetch({
+		endpoint: "/recipe?id_user=" + id,
+		method: "GET",
+	});
+
 	const user = getUser.data;
+	const recipes = getRecipes.data;
 
 	return (
 		<div className="profile">
@@ -25,9 +36,19 @@ function Profile() {
 				</div>
 			</>
 			)}
-			<div className="profile-recipes">
-				<h4>Voir Toutes vos recettes</h4>
-			</div>
+			<section className="profile-recipes">
+				<h4 className="profile-recipes__title"><IoRestaurantOutline />Toutes les recettes du chef :</h4>
+				<ul className="profile-recipes__list">
+					{recipes && 
+					recipes.map((recipe) => (	
+					<li key={recipe.id} className="profile-recipes__list--item">
+						<HeaderCard recipe={recipe} />
+						<IngredientsList ingredients={recipe.ingredients} />
+					</li>
+					))
+					}
+				</ul>
+			</section>
 		</div>
 	);
 }
