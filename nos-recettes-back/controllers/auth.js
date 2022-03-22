b26281f1c92ba2b433dadcaf70b7555d21e4c795
elsa <elsa.dessarps@gmail.com>
2022-03-22 12:48:55 +0100

    Fix (token)  : correct variable key for the token

--- nos-recettes-back/controllers/auth.js
+++ nos-recettes-back/controllers/auth.js
@@ -32,7 +32,7 @@ exports.login = (req, res, next) => {
 						id: user.id,
 						token: jwToken.sign(
 							// 1rst param : payload : data we wants to encode
-							{ userId: user.id, isAdmin: user.isAdmin, isActive: user.isActive},
+							{ userId: user.id, isAdmin: user.is_admin, isActive: user.is_active},
 							// 2nd param : secret key for encode
 							process.env.TOKEN_KEY,
 							// 3rd param : to configure duration of the token
